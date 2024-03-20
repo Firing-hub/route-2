18fe5f19c9f9bfa1d9860f97c138fa7363bdfd30
adhyant <adyantprakash03@gmail.com>
2024-03-21 03:56:22 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Header from "./component/header/Header"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Legal from "./pages/legal/Legal"
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 const App = () => {
+  const [user, setUser] = useState(null);
+  onAuthStateChanged(auth, (credentialUser) => {
+    setUser(credentialUser);
+  })
   return (
     <BrowserRouter>
     <Header></Header>
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/legal" element={<Legal />} />
+        <Route path="/legal" element={user ? <Legal /> : <Register/>} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
